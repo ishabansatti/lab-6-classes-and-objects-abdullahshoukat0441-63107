a85d8896b655d78997be1174d37af2327752918c
Muhammad Abdullah Shoukat <abdullahshoukat0441@gmail.com>
2026-03-30 23:00:04 +0500

    Add files via upload

---
+++ lab6task4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+public:
+    string name;
+    int age; 
+    Person() : name("Unknown"), age(0) {}
+  
+    Person(string n) : name(n), age(0) {}
+
+    Person(string n, int a) : name(n), age(a) {}
+
+    void display()
+     {
+        cout << "Name: " << name <<endl<< " Age: " << age << endl;
+    }
+};
+
+int main() {
+    Person p1;          
+    Person p2("Ali");   
+    Person p3("Sara", 20);
+    p1.display();
+    p2.display();
+    p3.display();
+    return 0;
+}
